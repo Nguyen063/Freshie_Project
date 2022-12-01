895dc02151929b2ffe29aec453cf5966002c554f
Nguyen063 <93272712+Nguyen063@users.noreply.github.com>
2022-12-02 05:04:42 +0700

    FIX lỗi
    - Đã fix lại trang rating đứa nào báo đó

--- app/src/main/java/com/phamvannguyen/freshie/rating/Rating.java
+++ app/src/main/java/com/phamvannguyen/freshie/rating/Rating.java
@@ -12,9 +12,6 @@ import com.phamvannguyen.freshie.R;
 
 public class Rating extends AppCompatActivity {
 
-    private TabLayout tabLayout;
-    private ViewPager2 viewPager2;
-    private RatingHistoryAdapter ratingHistoryAdapter;
 
     @SuppressLint("MissingInflatedId")
     @Override
@@ -22,22 +19,5 @@ public class Rating extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_rating);
 
-        tabLayout = findViewById(R.id.tab_layout_rating_history);
-        viewPager2 = findViewById(R.id.rating_history_viewpager);
-
-        ratingHistoryAdapter = new RatingHistoryAdapter(this);
-        viewPager2.setAdapter(ratingHistoryAdapter);
-
-        new TabLayoutMediator(tabLayout, viewPager2, (tab, position) -> {
-
-            switch (position) {
-                case 0:
-                    tab.setText("Chưa đánh giá");
-                    break;
-                case 1:
-                    tab.setText("Đã đánh giá");
-                    break;
-            }
-        }).attach();
     }
 }
--- app/src/main/java/com/phamvannguyen/freshie/rating/RatingHistory.java
+++ app/src/main/java/com/phamvannguyen/freshie/rating/RatingHistory.java
@@ -1,16 +1,41 @@
 package com.phamvannguyen.freshie.rating;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager2.widget.ViewPager2;
 
 import android.os.Bundle;
 
+import com.google.android.material.tabs.TabLayout;
+import com.google.android.material.tabs.TabLayoutMediator;
 import com.phamvannguyen.freshie.R;
 
 public class RatingHistory extends AppCompatActivity {
+
+    private TabLayout tabLayout;
+    private ViewPager2 viewPager2;
+    private RatingHistoryAdapter ratingHistoryAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_rating_history);
+
+        tabLayout = findViewById(R.id.tab_layout_rating_history);
+        viewPager2 = findViewById(R.id.rating_history_viewpager);
+
+        ratingHistoryAdapter = new RatingHistoryAdapter(this);
+        viewPager2.setAdapter(ratingHistoryAdapter);
+
+        new TabLayoutMediator(tabLayout, viewPager2, (tab, position) -> {
+
+            switch (position) {
+                case 0:
+                    tab.setText("Chưa đánh giá");
+                    break;
+                case 1:
+                    tab.setText("Đã đánh giá");
+                    break;
+            }
+        }).attach();
     }
 }
