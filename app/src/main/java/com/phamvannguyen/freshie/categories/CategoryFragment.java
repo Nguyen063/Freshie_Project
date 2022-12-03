1d2658460d225c44e5fe0d8576f57696b1c1a4be
KieuTrinh-T <96128479+KieuTrinh-T@users.noreply.github.com>
2022-12-03 16:07:10 +0700

    completed login, category in processing, change home

---
+++ app/src/main/java/com/phamvannguyen/freshie/categories/CategoryFragment.java
@@ -0,0 +1,16 @@
+package com.phamvannguyen.freshie.categories;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.phamvannguyen.freshie.R;
+
+public class CategoryFragment extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.fragment_category);
+    }
+}
--- app/src/main/java/com/phamvannguyen/freshie/login/LoginActivity.java
+++ app/src/main/java/com/phamvannguyen/freshie/login/LoginActivity.java
@@ -2,8 +2,11 @@ package com.phamvannguyen.freshie.login;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
+import com.phamvannguyen.freshie.MainActivity;
 import com.phamvannguyen.freshie.R;
 import com.phamvannguyen.freshie.databinding.ActivityLoginBinding;
 
@@ -16,6 +19,28 @@ public class LoginActivity extends AppCompatActivity {
 //        setContentView(R.layout.activity_login);
         binding = ActivityLoginBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+        addEvents();
+
+    }
+
+    private void addEvents() {
+        binding.btnLogin.setOnClickListener(new View.OnClickListener() {
+
+            //Allow user to login first regardless of the username and password
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        binding.tvRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(LoginActivity.this, SignupActivity.class);
+                startActivity(intent);
+            }
+        });
 
     }
 
