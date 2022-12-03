abfc78759276f4b70784afa92afc25f36acd659e
linhanhnguyen02 <anhntl20411c@st.uel.edu.vn>
2022-12-03 12:02:35 +0700

    Design Profile View

---
+++ app/src/main/java/com/phamvannguyen/freshie/profile/Profile.java
@@ -0,0 +1,19 @@
+package com.phamvannguyen.freshie.profile;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.text.Html;
+import android.widget.Button;
+
+import com.phamvannguyen.freshie.R;
+
+public class Profile extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+    }
+
+}
