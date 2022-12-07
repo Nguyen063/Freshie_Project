34bd116854b944a73c82c642f0aceb1acc29376f
KieuTrinh-T <96128479+KieuTrinh-T@users.noreply.github.com>
2022-12-07 23:01:05 +0700

    psf INTENT_NAME for ListProduct

--- app/src/main/java/com/phamvannguyen/freshie/product/ListProductActivity.java
+++ app/src/main/java/com/phamvannguyen/freshie/product/ListProductActivity.java
@@ -5,12 +5,18 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.phamvannguyen.freshie.R;
+import com.phamvannguyen.freshie.databinding.ActivityListProductBinding;
 
 public class ListProductActivity extends AppCompatActivity {
 
+    ActivityListProductBinding binding;
+    public static final String INTENT_FILTER = "filter";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_list_product);
+//        setContentView(R.layout.activity_list_product);
+        binding = ActivityListProductBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
     }
+
 }
